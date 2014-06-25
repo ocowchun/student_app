d34d2ba3d6227939dccc66bd6e8c2f878c192a55
ocowchun <ocowchun@gmail.com>
2014-06-25 16:03:08 +0800

    New: add avatar to students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -69,6 +69,6 @@ class StudentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def student_params
-      params.require(:student).permit(:name, :phone, :email)
+      params.require(:student).permit(:name, :phone, :email,:avatar)
     end
 end
